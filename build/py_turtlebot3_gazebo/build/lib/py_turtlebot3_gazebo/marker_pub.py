1704b69a7443942bd06506dbf779213355f0de8c
shiotoriharuki <underbird.h@gmail.com>
2020-07-21 00:38:04 +0900

    MarkerArrayができた

--- build/py_turtlebot3_gazebo/build/lib/py_turtlebot3_gazebo/marker_pub.py
+++ build/py_turtlebot3_gazebo/build/lib/py_turtlebot3_gazebo/marker_pub.py
@@ -1,6 +1,9 @@
 import rclpy
 from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 from rclpy.node import Node
+
+from builtin_interfaces.msg import Duration
 
 
 class MyMarker(Node):
@@ -13,45 +16,51 @@ class MyMarker(Node):
 
         self.get_logger().info("%s initializing..." % (self.TOPIC))
 
-        self.pub = self.create_publisher(Marker, self.TOPIC, 10)
+        self.pub = self.create_publisher(MarkerArray, self.TOPIC, 10)
 
-        timer_period = 0.5  # seconds
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         marker_data = Marker()
-        marker_data.header.frame_id = "map"
-        marker_data.header.stamp = rclpy.time
+        marker_array = MarkerArray()
 
-        marker_data.ns = "basic_shapes"
-        marker_data.id = 0
+        for i in range(5):
+            marker_data.header.frame_id = "map"
+            #marker_data.header.stamp = rclpy.time
 
-        marker_data.action = Marker.ADD
+            marker_data.ns = "basic_shapes"
+            marker_data.id = i
 
-        marker_data.pose.position.x = 0.0
-        marker_data.pose.position.y = 0.0
-        marker_data.pose.position.z = 0.0
+            marker_data.action = Marker.ADD
 
-        marker_data.pose.orientation.x=0.0
-        marker_data.pose.orientation.y=0.0
-        marker_data.pose.orientation.z=1.0
-        marker_data.pose.orientation.w=0.0
+            marker_data.pose.position.x = 0.0
+            marker_data.pose.position.y = 0.0
+            marker_data.pose.position.z = float(i)
 
-        marker_data.color.r = 1.0
-        marker_data.color.g = 0.0
-        marker_data.color.b = 0.0
-        marker_data.color.a = 1.0
+            marker_data.pose.orientation.x=0.0
+            marker_data.pose.orientation.y=0.0
+            marker_data.pose.orientation.z=1.0
+            marker_data.pose.orientation.w=0.0
 
-        marker_data.scale.x = 1.
-        marker_data.scale.y = 0.1
-        marker_data.scale.z = 0.1
+            marker_data.color.r = 1.0
+            marker_data.color.g = 0.0
+            marker_data.color.b = 0.0
+            marker_data.color.a = 1.0
 
-        #marker_data.lifetime = rclpy.Duration
+            marker_data.scale.x = 1.
+            marker_data.scale.y = 0.1
+            marker_data.scale.z = 0.1
 
-        marker_data.type = 0
+            marker_data.lifetime = Duration()
+
+            marker_data.type = 0
+
+            marker_array.markers.append(marker_data)
     
-        self.pub.publish(marker_data)
-        self.get_logger().info('Publishing: "%s"' % marker_data)
+            self.pub.publish(marker_array)
+
+        self.get_logger().info('Publishing: "%s"' % marker_array.markers[0].pose)
 
 
 
