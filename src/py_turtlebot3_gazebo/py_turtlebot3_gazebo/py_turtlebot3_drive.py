c8b39958cbed68c70cf6f466e8c30fdf146342b3
shiotoriharuki <underbird.h@gmail.com>
2020-07-22 00:12:15 +0900

    driveで矢印表示と時間取得ができた

--- src/py_turtlebot3_gazebo/py_turtlebot3_gazebo/py_turtlebot3_drive.py
+++ src/py_turtlebot3_gazebo/py_turtlebot3_gazebo/py_turtlebot3_drive.py
@@ -9,6 +9,11 @@ from rclpy.qos import qos_profile_system_default
 
 import numpy as np
 
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+from builtin_interfaces.msg import Duration
+from scipy.spatial.transform import Rotation
+
 class Turtlebot3Drive(Node):
     # ノード名
     #SELFNODE = "cmd_vel"
@@ -16,12 +21,19 @@ class Turtlebot3Drive(Node):
     TOPIC_VEL = "cmd_vel"
     TOPIC_SCAN = "scan"
     TOPIC_ODOM = "odom"
+    TOPIC_MARKER = "marker"
+
+    flag = True
 
     def __init__(self):
         # variable define
         self.scan_data = np.zeros(3)
         self.poses = np.zeros(3)
         self.orientations = np.zeros(4)
+
+        self.cova_l = np.zeros(3)
+        self.cova_a = np.zeros(3)
+        self.time = 0
 
         # ノードの初期化
         super().__init__(self.TOPIC_VEL)
@@ -35,6 +47,7 @@ class Turtlebot3Drive(Node):
 
         # publisherインスタンスを生成
         self.pub = self.create_publisher(Twist, self.TOPIC_VEL, 10)
+        self.marker_publisher = self.create_publisher(MarkerArray, self.TOPIC_MARKER, 10)
 
         profile = qos_profile_system_default
 
@@ -75,27 +88,78 @@ class Turtlebot3Drive(Node):
         self.orientations[2] = msg.pose.pose.orientation.z
         self.orientations[3] = msg.pose.pose.orientation.w
 
+        self.cova_l[0] = msg.twist.twist.linear.x
+        self.cova_l[1] = msg.twist.twist.linear.y
+        self.cova_l[2] = msg.twist.twist.linear.z
+
+        self.time = msg.header.stamp.sec
+
 
         #self.get_logger().info("odom %s" % msg)
-        self.get_logger().info("odom %f" % self.poses[0])
+        #self.get_logger().info("odom %f" % self.poses[0])
+        self.get_logger().info("x %f" % self.cova_l[0]+ "y %f" % self.cova_l[1] + "z %f" % self.cova_l[2])
+        self.get_logger().info("time %d" % self.time)
 
     def callback(self):
         """
         タイマーの実行部
         """
-        self.get_logger().info("Publish [%s]" % (self.count))
+        #self.get_logger().info("Publish [%s]" % (self.count))
         #self.get_logger().info("hello %f" % (scan_data))
 
+        #--------------cmd_velo publish----------------#
         # 送信するメッセージの作成
         cmd_vel = Twist()
-        cmd_vel.linear.x = 0.
-        cmd_vel.angular.z = 0.6
+        cmd_vel.linear.x = 0.1
+        cmd_vel.angular.z = 0.1
         #msg.value = self.count
         
         # 送信
         self.pub.publish(cmd_vel)
         # カウンタをインクリメント
-        self.count += 1.
+        #self.count += 1.
+
+
+        #---------------particle publich--------------#
+        marker_data = Marker()
+        marker_array = MarkerArray()
+        rot = Rotation.from_rotvec(np.array([0, 0, np.pi/3]))
+        quo = rot.as_quat()
+
+        for i in range(5):
+            marker_data.header.frame_id = "odom"
+            #marker_data.header.stamp = rclpy.time
+
+            marker_data.ns = "basic_shapes"
+            marker_data.id = i
+
+            marker_data.action = Marker.ADD
+
+            marker_data.pose.position.x = 0.0
+            marker_data.pose.position.y = 0.0
+            marker_data.pose.position.z = float(i)
+
+            marker_data.pose.orientation.x = quo[0]
+            marker_data.pose.orientation.y = quo[1]
+            marker_data.pose.orientation.z = quo[2]
+            marker_data.pose.orientation.w = quo[3]
+
+            marker_data.color.r = 1.0
+            marker_data.color.g = 0.0
+            marker_data.color.b = 0.0
+            marker_data.color.a = 1.0
+
+            marker_data.scale.x = 0.5
+            marker_data.scale.y = 0.05
+            marker_data.scale.z = 0.05
+
+            marker_data.lifetime = Duration()
+
+            marker_data.type = 0
+
+            marker_array.markers.append(marker_data)
+    
+            self.marker_publisher.publish(marker_array)
 
     
         
